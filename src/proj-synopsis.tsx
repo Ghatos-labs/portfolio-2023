993373afe21c936c39b8aa51bfcef4754362b66d
Ghatos1 <marwan.ghatos.akkari@gmail.com>
2023-12-01 03:48:29 +0100

    proj-synopsis content is now imported dynamically

--- src/proj-synopsis.tsx
+++ src/proj-synopsis.tsx
@@ -8,62 +8,17 @@ interface Props {
 }
 
 function ProjSynopsys(props: Props) {
-  var project = null;
-  if(props.project == "hexword")
-  {
-    project = <div className="main-div about-div">
-          <ReturnBtn link="/projects"/>
-          <h2>{Text.project.hexword.title}</h2>
-          <img src={Text.project.hexword.imageLink} className="project-img"></img>
-          <p>{Text.project.hexword.description}</p>
-      </div>
-  }
-  else if(props.project == "scalperShooter")
-  {
-    project = <div className="main-div about-div">
-          <ReturnBtn link="/projects"/>
-          <h2>{Text.project.scalperShooter.title}</h2>
-          <img src={Text.project.scalperShooter.imageLink} className="project-img"></img>
-          <p>{Text.project.scalperShooter.description}</p>
-      </div>
-  }
-  else if(props.project == "cvVideo")
-  {
-    project = <div className="main-div about-div">
-          <ReturnBtn link="/projects"/>
-          <h2>{Text.project.cvVideo.title}</h2>
-          <img src={Text.project.cvVideo.imageLink} className="project-img"></img>
-          <p>{Text.project.cvVideo.description}</p>
-      </div>
-  }
-  else if(props.project == "fallingShapes")
-  {
-    project = <div className="main-div about-div">
-          <ReturnBtn link="/projects"/>
-          <h2>{Text.project.fallingShapes.title}</h2>
-          <img src={Text.project.fallingShapes.imageLink} className="project-img"></img>
-          <p>{Text.project.fallingShapes.description}</p>
-      </div>
-  }
-  else if(props.project == "tacticalNounours")
-  {
-    project = <div className="main-div about-div">
-          <ReturnBtn link="/projects"/>
-          <h2>{Text.project.tacticalNounours.title}</h2>
-          <img src={Text.project.tacticalNounours.imageLink} className="project-img"></img>
-          <p>{Text.project.tacticalNounours.description}</p>
-      </div>
-  }
-  else if(props.project == "depletime")
-  {
-    project = <div className="main-div about-div">
-          <ReturnBtn link="/projects"/>
-          <h2>{Text.project.depletime.title}</h2>
-          <img src={Text.project.depletime.imageLink} className="project-img"></img>
-          <p>{Text.project.depletime.description}</p>
-      </div>
-  }
-  return project;
+  //this line throws an error but the code does works, so...
+  const { title, imageLink, description } = Text.project[props.project];
+
+  return (
+    <div className="main-div about-div">
+      <ReturnBtn link="/projects"/>
+      <h2 key={props.project}>{title}</h2>
+      <img key={props.project} src={imageLink} className="project-img" alt={title}></img>
+      <p key={props.project}>{description}</p>
+    </div>
+  );
 }
 
 export default ProjSynopsys
