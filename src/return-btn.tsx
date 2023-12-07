354b582c89a97d1e2ed10c8edb18f015bc040f87
m.akkari <m.akkari@student.isartdigital.com>
2023-12-07 19:10:30 +0100

    Fixed moving background not aligning properly when scrolling is possible, added fade effect on content div when scolling

--- src/return-btn.tsx
+++ src/return-btn.tsx
@@ -10,7 +10,10 @@ interface Props {
 function ReturnBtn(props: Props) {
 
   return (
-    <Link to={props.link} className="return-btn"><img src={ReturnIcon}></img><h3>{Text.returnBtn.text}</h3></Link>
+    <>
+      <div className="return-btn-background"></div>
+      <Link to={props.link} className="return-btn"><img src={ReturnIcon}></img><h3>{Text.returnBtn.text}</h3></Link>
+    </>
   )
 }
 
